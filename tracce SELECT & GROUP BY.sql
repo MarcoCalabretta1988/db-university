e4b446c8616774dbf192ef5a32f462981dfdc3b2
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-02-14 13:37:06 +0100

    3 query ended

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -5,10 +5,10 @@
 SELECT `name`,`surname`,`date_of_birth` FROM `students` WHERE YEAR(`date_of_birth`) = 1990;
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
 
-
+SELECT `name`,`cfu` FROM `courses` WHERE `cfu` > 10;
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 
-
+SELECT `name`,`surname`,`date_of_birth` FROM `students` WHERE YEAR(`date_of_birth`) >= '1993';
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
 
