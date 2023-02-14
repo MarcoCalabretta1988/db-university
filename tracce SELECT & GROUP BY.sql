84648d12a382e31b385414baf7d72475c13ae8a5
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-02-14 13:47:23 +0100

    7 query ended

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -20,7 +20,7 @@ SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND `hour` >= '14:00:00';
 SELECT `name`,`level` FROM `degrees` WHERE `level` = 'magistrale';
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 
-
+SELECT COUNT(*) AS 'Number of deparment' FROM `departments`;
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
 
