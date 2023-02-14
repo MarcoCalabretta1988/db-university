439fed234a6ffc2dab3098a0c1c37ddf920c5c25
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-02-14 13:40:46 +0100

    3 Query ended

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -11,7 +11,7 @@ SELECT `name`,`cfu` FROM `courses` WHERE `cfu` > 10;
 SELECT `name`,`surname`,`date_of_birth` FROM `students` WHERE YEAR(`date_of_birth`) >= '1993';
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
-
+SELECT `name`,`period`,`year` FROM `courses` WHERE `period` = 'I semestre' AND `year` = '1';
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
 
