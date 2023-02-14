09000171ee4a048202de4ae031089d49cd0511e9
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-02-14 13:43:57 +0100

    5 query ended

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -14,7 +14,7 @@ SELECT `name`,`surname`,`date_of_birth` FROM `students` WHERE YEAR(`date_of_birt
 SELECT `name`,`period`,`year` FROM `courses` WHERE `period` = 'I semestre' AND `year` = '1';
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
-
+SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND `hour` >= '14:00:00';
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 
 
