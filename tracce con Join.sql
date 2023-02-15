291b9fb43a2ddf46ebc208c9f00c193113074125
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-02-15 13:09:39 +0100

    Query 5 completed

--- tracce con Join.sql
+++ tracce con Join.sql
@@ -27,9 +27,25 @@ AND `teachers`.`surname` = 'Amato';
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 
+SELECT `students`.`name`, `students`.`surname`,`departments`.`name` AS 'Department',`degrees`.`name` AS 'Degree',`degrees`.`level` AS 'Level',`degrees`.`website` AS 'Website'
+FROM `students`
+JOIN `degrees`
+ON `degrees`.`id` = `students`.`degree_id`
+JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+ORDER BY `students`.`surname`, `students`.`name` ASC;
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
+SELECT `courses`.`name` AS 'Course', `degrees`.`name` AS 'Degree',`teachers`.`name`,`teachers`.`surname`
+FROM `degrees`
+JOIN `courses`
+ON `courses`.`degree_id` = `degrees`.`id`
+JOIN `course_teacher`
+ON `course_teacher`.`course_id`= `courses`.`id`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+ORDER BY `degrees`.`name`;
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 
