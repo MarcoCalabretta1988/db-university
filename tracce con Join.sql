539d7c561a39ac9e55827992a5c5d16c7cf99fcb
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-02-15 12:50:23 +0100

    Query 3 completed

--- tracce con Join.sql
+++ tracce con Join.sql
@@ -16,6 +16,14 @@ WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
+SELECT `courses`.`name`
+FROM `courses`
+JOIN `course_teacher`
+ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+WHERE `teachers`.`name` = 'Fulvio'
+AND `teachers`.`surname` = 'Amato';
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 
