c7428d87f6afad192540f0a72a8f871deb505bf3
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-02-15 13:16:39 +0100

    Query 6 completed

--- tracce con Join.sql
+++ tracce con Join.sql
@@ -49,6 +49,17 @@ ORDER BY `degrees`.`name`;
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 
+SELECT DISTINCT `teachers`.`name`, `teachers`.`surname`
+FROM `departments`
+JOIN `degrees`
+ON `degrees`.`department_id` = `departments`.`id`
+JOIN `courses`
+ON `courses`.`degree_id` = `degrees`.`id`
+JOIN `course_teacher`
+ON `course_teacher`.`course_id`= `courses`.`id`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+WHERE `departments`.`name` = 'Dipartimento di Matematica';
 
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
 
